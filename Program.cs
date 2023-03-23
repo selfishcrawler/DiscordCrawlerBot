7f4c5ffa813b6bf657972f2d4aaef97b8210aa23
SelfishCrawler <goblyashik@gmail.com>
2023-03-23 23:54:05 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DiscordCrawlerBot;
+
+internal class Program
+{
+    private static string _token;
+
+    static void Main(string[] args)
+    {
+        var cfg = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        _token = cfg["CrawlerDiscord"];
+    }
+}
